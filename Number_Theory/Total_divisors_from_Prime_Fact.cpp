0ae8b3b46e94a1fdaca574b4535531664ba41892
Ronak Surana <ronaksurana2017@gmail.com>
2022-03-20 22:25:16 +0530

    Updated Total_Div_file

--- Number_Theory/Total_divisors_from_Prime_Fact.cpp
+++ Number_Theory/Total_divisors_from_Prime_Fact.cpp
@@ -1,3 +1,8 @@
+//N=P1^y1*P2^y2*P3^y3*.......Pn^yn
+//Total Divisors=(y1+1)*(y2+1)*...(yn+1)
+//Ex:N=300
+//N=2^2*3^1*5^2
+//Total Divisors=(2+1)*(1+1)*(2+1)=3*2*3=18
 #include<bits/stdc++.h>
 using namespace std;
 int primeFact(int n)
