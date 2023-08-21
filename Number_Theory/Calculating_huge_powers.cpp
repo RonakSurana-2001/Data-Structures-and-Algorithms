5a28a8f5731475cd6afba25913985d02a1405e47
Ronak Surana <100184606+RonakSurana-2001@users.noreply.github.com>
2023-08-21 14:39:46 +0530

    Create Calculating_huge_powers.cpp

---
+++ Number_Theory/Calculating_huge_powers.cpp
@@ -0,0 +1,31 @@
+//m must be prime
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ll findPower(ll a,ll n,ll m)
+{
+    ll res=1;
+    while(n>0)
+    {
+        if(n%2!=0)
+        {
+            res=res*a%m;
+            n--;
+        }
+        else{
+          a=a*a%m;
+          n=n>>1;
+        }
+    }
+    return res;
+}
+
+int main() 
+{
+  ll a,b,c;cin>>a>>b>>c;
+  ll m=1e9+7;
+  ll power1=findPower(b,c,m-1);
+  ll power2=findPower(a,power1,m);
+  cout<<power2<<endl;
+}
